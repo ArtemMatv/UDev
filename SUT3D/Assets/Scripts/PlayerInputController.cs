de06d5c234ec2848ef92a0be60c5d60bedd82271
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-30 21:38:33 +0300

    created map

---
+++ SUT3D/Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    [SerializeField] private PlayerController _player;
+    void Update()
+    {
+        
+    }
+}
