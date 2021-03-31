b81d4ef588608085046afa6b92f34f569ac506a4
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2021-03-31 16:02:12 +0300

    deleted wrong project

--- SUT3D/Assets/Scripts/Interactable/Enemy/MonstersController.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class MonstersController : EnemyBaseController
-{
-    [SerializeField] private Detachment _monsters;
-
-    public Detachment Monsters => _monsters;
-}
